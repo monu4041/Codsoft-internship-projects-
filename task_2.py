f1ce54db0828ec995bee3e6f4f375a510c081d9f
Monu Prajapati <pmonu4041@gmail.com>
2024-09-03 22:11:45 +0530

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,16 @@
+# Simple Calculator Projects
+
+a=int (input("Enter the 1st value :"))
+b=int (input("Enter the 2nd value :"))
+x=input("enter the Operator(+,-,*,/).....")
+if x=="+": #(a+b,a-b,a*b,a/b):
+    print ("Addition:",a+b)
+elif x=="-":
+    print ("Subtraction:",a-b)
+elif x== "*":
+    print ("Multiplication :",a*b)
+elif x=="/":
+    print ("Division :",a/b)
+else:
+    print ("invalid Operator")
+    
